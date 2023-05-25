971df05bf244cdadca4b2c1db60371586aae08a0
Wettergrund <jonas.wettergrund@gmail.com>
2023-05-25 16:22:40 +0200

    Changed code and test result is OK.

--- WordTDD/Worder.cs
+++ WordTDD/Worder.cs
@@ -2,13 +2,30 @@
 {
     public class Worder
     {
+        private string _word;
         public Worder(string testWord)
         {
+            _word = testWord;
+
         }
 
         public bool IsPalindrome()
         {
-            throw new NotImplementedException();
+            _word = _word.Replace(" ", "");
+
+            int position = 0;
+            int lettersLeft = _word.Length - 1;
+
+            while (position < lettersLeft)
+            {
+                if (_word[position] != _word[lettersLeft])
+                    return false;
+
+                position++;
+                lettersLeft--;
+            }
+
+            return true;
         }
     }
 }
